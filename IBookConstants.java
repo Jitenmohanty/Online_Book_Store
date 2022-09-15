08c1f0c00285a98b8f218cbf316f0be9daabfad1
Jiten Kumar Mohanty <105143946+Jitenmohanty@users.noreply.github.com>
2022-09-15 19:58:57 +0530

    Add files via upload

---
+++ IBookConstants.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sql;
+
+/**
+ *
+ * @author o
+ */
+public interface IBookConstants {
+	
+    public static String TABLE_BOOK = "books";
+	
+	public static String COLUMN_NAME = "name";
+	public static String COLUMN_BARCODE = "barcode";
+	public static String COLUMN_AUTHOR = "author";
+	public static String COLUMN_PRICE = "price";
+	public static String COLUMN_QUANTITY = "quantity";
+	public static String COLUMN_id = "id";
+}
+
